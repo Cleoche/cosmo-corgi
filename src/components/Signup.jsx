eb7fdf2ce611d70723200773243565b4e12e297a
Cleo Thor <kiszo.r6@gmail.com>
2024-11-02 21:06:01 -0400

    dynamic sign in

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -7,64 +7,23 @@ import polygon3 from "./assets/Polygon 3.svg";
 import polygon4 from "./assets/Polygon 4.svg";
 import "./stylesheets/signup.css";
 import useWindowDimensions from "./WindowDimensions.jsx";
+import Background from "./Background.jsx";
 
 export default function Signup() {
 const dimensions = useWindowDimensions();
     return (
         <div className="sign-in" style={{zoom: Math.min((dimensions.width / 1280), (dimensions.height / 832))}}>
             <div className="overlap-wrapper">
+                <Background/>
                 <div className="overlap">
 
                     <div className="overlap-2">
-                        <div className="ellipse-7"/>
-
-                        <div className="ellipse-8"/>
-
-                        <div className="ellipse-9"/>
-
-                        <div className="ellipse-7"/>
-
-                        <div className="ellipse-8"/>
-
-                        <div className="ellipse-9"/>
 
                         <div className="text-wrapper-4">Sign in</div>
                     </div>
 
                     <div className="overlap-group">
-                        <div className="ellipse"/>
 
-                        <div className="div"/>
-
-                        <img className="polygon" alt="Polygon" src={polygon1}/>
-
-                        <img className="img" alt="Polygon" src={polygon2}/>
-
-                        <div className="ellipse-2"/>
-
-                        <img className="ellipse-3" alt="Ellipse" src={ellipse3}/>
-
-                        <div className="ellipse-4"/>
-
-                        <div className="ellipse-5"/>
-
-                        <div className="ellipse-6"/>
-
-                        <div className="ellipse"/>
-
-                        <img className="polygon" alt="Polygon" src={polygon3}/>
-
-                        <img className="img" alt="Polygon" src={polygon4}/>
-
-                        <div className="ellipse-2"/>
-
-                        <img className="ellipse-3" alt="Ellipse" src={ellipse12}/>
-
-                        <div className="ellipse-4"/>
-
-                        <div className="ellipse-5"/>
-
-                        <div className="ellipse-6"/>
 
                         <div className="rectangle"/>
 
